06a3a842c64b5209c8b7aedf909a282cd02bedd2
Kamil Rytarowski <n54@gmx.com>
2019-04-14 03:58:55 +0200

    fix

--- compat/misc.h
+++ compat/misc.h
@@ -3,5 +3,8 @@
 
 #define setresgid(a,b,c) setgid(a)
 #define setresuid(a,b,c) setuid(a)
+#define nitems(_a)      (sizeof((_a)) / sizeof((_a)[0]))
+
+static inline int pledge(const char *a, const char *b){return 0;}
 
 #endif
--- vmm.c
+++ vmm.c
@@ -16,6 +16,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "compat/misc.h"
 #include <sys/param.h>	/* nitems */
 #include <sys/ioctl.h>
 #include <sys/queue.h>
